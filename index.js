70fa6bd454327c1bf9f0626da1bf478ecfd46553
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-01-06 15:46:15 -0300

    created schemas

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
 const { gql, ApolloServer } = require("apollo-server")
 
+const usersMock = [
+    {
+        id: "1",
+        name: "John",
+        age: 30,
+        active: true,
+        salary: 1000
+    },
+    {
+        id: "2",
+        name: "Jane",
+        age: 25,
+        active: false,
+        salary: 2000
+    }
+]
+
+const productsMock = [{
+    id: "1",
+    name: "Product 1",
+    price: 100.00,
+    description: "This is a product"
+},
+{
+    id: "2",
+    name: "Product 2",
+    price: 200.00,
+    description: "This is a product"
+}]
+
 const typeDefs = gql`
-    type Query {
+
+    type Product {
+        id: ID
+        name: String
+        price: Float
+        description: String
+    }
+
+    type User {
         id: ID
         name: String
         age: Int
         active: Boolean
         salary: Float
-        tech: [String!]!
+    }
+
+    type Query {
+        users: [User]
+        products: [Product]
     }
 `
 
 const resolvers = {
     Query: {
-        id: () => "123",
-        name: () => "John Doe",
-        age: () => 30,
-        active: () => true,
-        salary: () => 30000.00,
-        tech: () => ["React", "Node", "GraphQL"]
+        users: () => usersMock,
+        products: () => productsMock
     }
 }
 
