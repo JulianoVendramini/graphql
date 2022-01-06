3f3015446287981ae0209ac4230de04a78c605ce
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-01-06 10:53:32 -0300

    hello world

---
+++ index.js
@@ -0,0 +1,20 @@
+const { gql, ApolloServer } = require("apollo-server")
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello World"
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen()
